c770dc5280a223a55bc49284e82ae3d5c5eae2c7
hh54188 <252438823@qq.com>
2017-03-16 16:35:56 +0800

    update

---
+++ src/components/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class InputComponent extends React.Component {
+    render() {
+        return (
+            <input type="text" />
+        );
+    }
+}
+
+export default InputComponent;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,13 @@
 
 import React from 'react';
-import { Container, Form, Input, Checkbox } from 'semantic-ui-react'
-require('semantic-ui-css/semantic.min.css');
+import Input from './Input';
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <Container>
-        <Form>
-          <Form.Field inline>
-            <label>文件夹路径</label>
-            <Input value="Helo" />
-          </Form.Field>
-          <Form.Field inline>
-            <label>端口号</label>
-            <Input value="Helo" />            
-          </Form.Field>
-          <Form.Field inline>
-            <label>启用production目录</label>
-            <Checkbox toggle label="" />
-          </Form.Field>          
-        </Form>
-      </Container>
+      <Input />
     );
   }
 }
-
-AppComponent.defaultProps = {
-};
 
 export default AppComponent;
